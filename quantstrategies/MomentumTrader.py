e1210e5c0301e068f22ca5444947ab6ec1b60ae8
mnash29 <mtntrades10@gmail.com>
2023-08-17 16:32:04 -0700

    Added example for analyzing trading costs

--- quantstrategies/MomentumTrader.py
+++ quantstrategies/MomentumTrader.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 
 from datetime import datetime, timedelta
+from operator import itemgetter
 
 import tpqoa
 
@@ -23,21 +24,40 @@ class MomentumTrader(tpqoa.tpqoa):
         The number of point in the tick data for establishing a moving average
     units: int
         The number of units to use when entering/exiting a position
+    features: list
+        The list of feature columns for use in model prediction
+    model: model
+        The trained machine learning model
+    dev: int
+        The number of deviations away from the trendline for use in the Bollinger Band
+        strategy, default=2
+    SMA: tuple
+        The tuple of window lengths for calculating smooth moving averages. The first two
+        values are used for evaluating a SMA cross over strategy and the third is used for
+        the Bollinger Band middle trend line, default=(50, 200, 20)
+    EMA: tuple
+        The typle of window lengths for calculating stacked EMA's, default=(8, 21, 34, 55, 89)
     """
 
-    def __init__(self, conf_file, instrument, bar_length, window, units):
+    def __init__(self, conf_file, instrument,
+                 bar_length, window, units,
+                 dev=2, SMA=(50, 200, 20)):
         super().__init__(conf_file)
 
         self.instrument = instrument
         self.bar_length = pd.to_timedelta(bar_length)
         self.tick_data = pd.DataFrame()
         self.raw_data = pd.DataFrame()
-        self.data = None
+        self.data = pd.DataFrame()
         self.last_bar = None
 
+        # SMA and Bollinger band strategy attributes
+        self.SMA = SMA
+        self.deviation = dev
+
+        # Contrarian strategy attributes
         self.window = window
         self.units = units
-
 
     def get_most_recent(self, days=5):
         """Retreive the most recent trading tick data.
@@ -64,6 +84,7 @@ class MomentumTrader(tpqoa.tpqoa):
                 self.bar_length, label="right").last().dropna().iloc[:-1]
 
             self.raw_data = df.copy()
+            self.data = df.copy()
             self.last_bar = self.raw_data.index[-1]
 
             if pd.to_datetime(datetime.utcnow()).tz_localize("UTC") - self.last_bar < self.bar_length:
@@ -73,12 +94,11 @@ class MomentumTrader(tpqoa.tpqoa):
         super().on_success(time, bid, ask)
 
         recent_tick = pd.to_datetime(time)
-        df = pd.DataFrame({self.instrument: (ask + bid)/1}, index=[recent_tick])
+        df = pd.DataFrame({self.instrument: (ask + bid)/2}, index=[recent_tick])
         self.tick_data = pd.concat([self.tick_data, df])
 
         if recent_tick - self.last_bar >= self.bar_length:
             self.resample_and_join()
-            self.define_con_strategy()
 
     def resample_and_join(self):
         """Resample the tick data and append to the raw data."""
@@ -88,11 +108,111 @@ class MomentumTrader(tpqoa.tpqoa):
         self.tick_data = self.tick_data[-1:]
         self.last_bar = self.raw_data.index[-1]
 
-    def define_con_strategy(self):
-        """Define the basic contrarian trading strategy."""
+    def merge_dataframes(self, df: pd.DataFrame):
+        """Merge the strategy data with the current dataset.
+
+        Args:
+        ----
+        df : pd.DataFrame
+            The new DataFrame with strategy specific columns
+        """
+        df.drop(self.instrument, axis=1, inplace=True)
+        self.data = pd.merge(self.data, df, on='time')
+
+    def eval_con_strategy(self):
+        """Evaluate tick data using the basic contrarian trading strategy."""
+        if self.data is None:
+            raise ValueError("Make call to get_recent_data() or stream_data() before evaluating.")
+
         df = self.raw_data.copy()
 
         df['Returns'] = np.log(df[self.instrument] / df[self.instrument].shift())
-        df['ConPosition'] = -np.sign(df.Returns.rolling(self.window).mean())
+        df['Position_Con'] = -np.sign(df.Returns.rolling(self.window).mean())
 
-        self.data = df.copy()
+        self.merge_dataframes(df)
+
+    def eval_sma_stategy(self):
+        """Evaluate tick data using a basic SMA crossover strategy."""
+        if len(self.SMA) < 3:
+            raise ValueError("Invalid values set for SMA attribute")
+
+        if self.data is None:
+            raise ValueError("Make call to get_recent_data() or stream_data() before evaluating.")
+
+        df = self.raw_data.copy()
+
+        df['SMA_S'] = df[self.instrument].rolling(self.SMA[0]).mean()
+        df['SMA_L'] = df[self.instrument].rolling(self.SMA[1]).mean()
+
+        df['Position_SMA'] = np.where(df.SMA_S > df.SMA_L, 1, -1)
+
+        self.merge_dataframes(df)
+
+    def eval_bollband_strategy(self):
+        """Evaluate tick data using a Bollinger Band strategy."""
+        if len(self.SMA) < 3:
+            raise ValueError("Invalid values set for SMA attribute")
+
+        if self.data is None:
+            raise ValueError("Make call to get_recent_data() or stream_data() before evaluating.")
+
+        df = self.raw_data.copy()
+
+        df['BB_SMA'] = df[self.instrument].rolling(self.SMA[2]).mean()
+        df['BB_Lower'] = df.BB_SMA - (
+            df[self.instrument].rolling(self.SMA[2]).std() * self.deviation)
+        df['BB_Upper'] = df.BB_SMA + (
+            df[self.instrument].rolling(self.SMA[2]).std() * self.deviation)
+        df['Distance'] = df[self.instrument] - df.BB_SMA
+
+        df['Position_BB'] = np.where(df[self.instrument] < df.BB_Lower, 1, np.nan)
+        df['Position_BB'] = np.where(df[self.instrument] > df.BB_Lower, -1, df.Position_BB)
+        df['Position_BB'] = np.where(
+            df.Distance * df.Distance.shift() < 0, 0, df.Position_BB)
+        df['Position_BB'] = df.Position_BB.ffill().fillna(0)
+
+        self.merge_dataframes(df)
+
+    # def log_reg_predict(self, features: list, model: LogisticRegression):
+    #     """Output a target market direction given current tick data.
+
+    #     Args:
+    #     ----
+    #     features : list
+    #         The list of feature columns for use in class label prediction
+    #     model : LogisticRegression
+    #         The pre-trained machine learning model
+    #     """
+    #     df = self.raw_data.copy()
+    #     df = df.append(self.tick_data)
+
+    #     df['Returns'] = np.log(df[self.instrument] / df[self.instrument].shift())
+    #     df['Position_ML'] = model.predict(df[features])
+
+    #     return df
+
+
+    def report_trade(self, order, action):
+        """Display trade information.
+
+        TODO: Ideally this should log to a file or database
+
+        Args:
+        ----
+        order : dict
+            The executed order information
+        action : str
+            The trade action taken, e.g. 'GOING LONG, GOING SHORT'
+        """
+        time, units, price, pl = itemgetter("time", "units", "price", "pl")(order)
+
+        if self.profits is None:
+            self.profits = [pl]
+        else:
+            self.profits.append(pl)
+
+        cumpl = sum(self.profits)
+        print("\n" + 100 * "-")
+        print(f"{time} | {action}")
+        print(f"{time} | units={units} | price={price} | P&L={pl} | Cum. P&L={cumpl}")
+        print(100 * "-" + "\n")
--- quantstrategies/quantstrategies.py
+++ quantstrategies/quantstrategies.py
@@ -1,8 +1,39 @@
 """Main module."""
+import pickle
+import numpy as np
 from os.path import dirname, abspath, join
 
 from MomentumTrader import MomentumTrader
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 cfg_path = join(dirname(dirname(abspath(__file__))), "oanda.cfg")
 
-trader = MomentumTrader(cfg_path, "EUR_USD", "1min", window=1, units=100000)
+trader = MomentumTrader(cfg_path, "EUR_USD", "1min", window=1, units=100000, dev=1, SMA=(50, 200, 47))
+trader.get_most_recent()
+trader.eval_con_strategy()
+trader.eval_sma_stategy()
+trader.eval_bollband_strategy()
+
+trader.data['Direction'] = np.sign(trader.data.Returns.shift(-1))
+
+lags = 3
+cols = ['Position_Con', 'Position_SMA', 'Position_BB']
+# cols = []
+# for lag in range(1, lags + 1):
+#     col = "lag{}".format(lag)
+#     trader.data[col] = trader.data.Returns.shift(lag)
+#     cols.append(col)
+
+trader.data.dropna(inplace=True)
+
+model = LogisticRegression(C=1e6, max_iter=100000, multi_class="ovr")
+
+X_train, X_test, y_train, y_test = train_test_split(trader.data, trader.data.Direction, test_size=0.30, random_state=42, shuffle=False)
+
+model.fit(X_train[cols], y_train)
+scores = model.score(X_test[cols], y_test)
+print(scores)
+
+X_test['Pred'] = model.predict(X_test[cols])
+X_test.to_csv('model_preds.csv')
